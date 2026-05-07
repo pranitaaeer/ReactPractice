bbb45a0880dd142340a6a4218e49d724c5f6bf26
Pranita Aeer <aeerpranita@gmail.com>
2026-05-07 15:34:26 +0530

    Correct small mistake

--- 01.Props/src/components/Callback.jsx
+++ 01.Props/src/components/Callback.jsx
@@ -1,18 +1,16 @@
-import React,{usestate,useCallback} from 'react'
+import React,{useState,useCallback} from 'react'
 import Child from "./Child.jsx"
 export function callback () {
 
-  const [count, setCount]=usestate(0)
+  const [count, setCount]=useState(0)
   const cb=useCallback (() =>{
-    function handlclick() {
     console.log("clicked") 
-  }
   },[]) 
   
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={setCount()=>count+1}>
+      <button onClick={()=>setCount(count+1)}>
       increment</button>
       <Child  click={cb}/>
     </>
