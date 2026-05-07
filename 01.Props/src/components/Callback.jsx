b2dd7f43bcff89212383ae10beb66e29f77c6353
Pranita Aeer <aeerpranita@gmail.com>
2026-05-07 15:24:49 +0530

    Create Callback component with state and callback
    Implement a callback component with state management and child interaction.

---
+++ 01.Props/src/components/Callback.jsx
@@ -0,0 +1,20 @@
+import React,{usestate} from 'react'
+import Child from "./Child.jsx"
+export function callback () {
+
+  const [count, setCount]=usestate(0)
+  const cb=usecallback (() =>{
+    function handlclick() {
+    console.log("clicked") 
+  }
+  },[]) 
+  
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={setCount()=>count+1}>
+      increment</button>
+      <Child  click={cb}/>
+    </>
+  ) 
+}
