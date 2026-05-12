0bb57ad09d4b37b61bd75e837aa73e3346a34e1d
Pranita Aeer <aeerpranita@gmail.com>
2026-05-12 23:48:43 +0530

    Add PopupModal component with open/close functionality

--- 01.Props/src/Interview/PopupModal.jsx
+++ 01.Props/src/Interview/PopupModal.jsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import { useState } from 'react'
+
+function PopupModal() {
+  const [show, setShow] = useState(false)
+  
+  
+  
+
+  return (
+    <div>
+      <button onClick={()=>setShow(true)}>open</button>
+      { 
+        show &&(
+        <div>
+        <h1>hello Modal</h1> 
+        <button onClick={()=>setShow(false)}>close</button> 
+        </div>) 
+      }
+   
+    </div>
+  )
+}
+
+export default PopupModal
 
