5953b0aa0ed6152a466312133d370a6f249bf979
Pranita Aeer <aeerpranita@gmail.com>
2026-05-12 15:00:09 +0530

    Fix errors in Check.jsx component

--- 01.Props/src/components/Check.jsx
+++ 01.Props/src/components/Check.jsx
@@ -1,41 +1,47 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
 export function Check() {
-  const [items, setItems]=useState([
+  const [items, setItems] = useState([
     "React", "NodeJs", "Typescript"
-  ]) 
-  const [checked, setChecked]=useState([]) 
-  const handleAll=() =>{
-    if(checked.length===items.length)
-    {
-      setChecked([]) 
-    }else setChecked (items) 
-    
+  ])
+  const [checked, setChecked] = useState([])
+  
+  const handleAll = () => {
+    if (checked.length === items.length) {
+      setChecked([])
+    } else {
+      setChecked(items)
+    }
   }
-  const handleOne=(item) =>{
-    setItems([...items,item]) 
+  
+  const handleOne = (item) => {
+    if (checked.includes(item)) {
+      setChecked(checked.filter(elm => elm !== item))
+    } else {
+      setChecked([...checked, item])
+    }
   }
+  
   return (
     <>
-      <input
-        type="checkbox"
-        checked={checked.length===items.length}
-        onChange={handleall}
-      
-      />
-      select All
-      {items.map((elm, idx)=>{return (
-      <p>{elm}</p>
-      <input
-      type="checkbox"
-      checked={checked.includes(elm)}
-      onChange={()=>handleOne(elm)}         
-      />
-      )}}
+      <div>
+        <input
+          type="checkbox"
+          checked={checked.length === items.length}
+          onChange={handleAll}
+        />
+        select All
+      </div>
+      {items.map((elm, idx) => (
+        <div key={idx}>
+          <p>{elm}</p>
+          <input
+            type="checkbox"
+            checked={checked.includes(elm)}
+            onChange={() => handleOne(elm)}
+          />
+        </div>
+      ))}
     </>
-  ) 
-
-
-
-  
+  )
 }
